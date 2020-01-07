98c4f39a5048329db8c8bfe24fd5f59a632ce042
Thomas Bauer <thomas.bauer1@grenoble-inp.org>
2020-01-07 20:07:38 +0100

    Symmetry images are saved

--- demo/test_symmetries.cpp
+++ demo/test_symmetries.cpp
@@ -12,12 +12,18 @@ int main()
     Image img(s1);
 
     img.display("Lion", "Lion");
+    
     Image img2 = img.sym_y();
     img2.display("Symmetry along y", "Symmetry along y");
+    img2.save("symmetry_y");
+
     Image img3 = img.sym_x();
     img3.display("Symmetry along x", "Symmetry along x");
+    img3.save("symmetry_x");
+
     Image img4 = img.sym_xy();
     img4.display("Symmetry along the diagonal", "Symmetry along the diagonal");
+    img4.save("symmetry_xy");
 
     return 0;
 }
