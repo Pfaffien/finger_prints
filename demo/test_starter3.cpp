aa2549d313a4297368abc774150560b09bd98399
Clara <clara.bourgoin@grenoble-inp.org>
2020-01-08 13:22:25 +0100

    starter 3

---
+++ demo/test_starter3.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/core.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include "starter3.h"
+
+int main(){
+  cv::String s1( "../img/clean_finger.png" );
+  Image finger(s1);
+
+  finger.display("Clean finger", "Clean finger");
+  cv::Mat_<float> mat_rien(3,3);
+  mat_rien(2,2) = 1;
+  Image rien(mat_rien);
+
+  Image res = convolution(finger, rien);
+  res.display();
+  return 0;
+}
