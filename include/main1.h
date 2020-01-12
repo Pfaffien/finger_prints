e6335fb9c38f6d0f936be441c0a4375e843fb644
Clara <clara.bourgoin@grenoble-inp.org>
2020-01-12 20:32:48 +0100

    debut doxygen

--- include/main1.h
+++ include/main1.h
@@ -1,14 +1,34 @@
 #ifndef _MAIN1_H_
 #define _MAIN1_H_
 
+
+/**
+ * \file main1.h
+ * \brief Usefull functions to answer the part main 1
+ * \author Thomas.B Clara.B
+ * \version 0.1
+ * \date 01/12/20
+ */
+
+
 #include <iostream>
 #include <vector>
 #include <opencv2/core.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 
-
+/**
+ * \fn float c_isotropic (float, float param = 1)
+ * \brief Definition of an isotropic function
+ * \return Result of the isotropic function
+ */
 float c_isotropic(float, float param = 1);
+
+/**
+ * \fn float c_anisotropic (int, int, cv::Point, float)
+ * \brief Definition of an anisotropic function
+ * \return Result of the anisotropic function
+ */
 float c_anisotropic(int, int, cv::Point, float);
 
 //param = 1: white by default
