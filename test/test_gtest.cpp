31b7dcee31a5ce29f3c05195f25d06e97450e328
Thomas Bauer <thomas.bauer1@grenoble-inp.org>
2020-01-12 11:16:26 +0100

    First tests on the class Image

--- test/test_gtest.cpp
+++ test/test_gtest.cpp
@@ -6,13 +6,53 @@
 
 #include "Image.h"
 
-TEST(gtest, test1)
+
+TEST(gtest, pixel_getter)
 {
-    cv::Mat_<float> mat(10, 15);
+    cv::Mat_<float> mat(10, 15, 1);
     Image test(mat);
-    EXPECT_EQ(test().rows, 10);
-    EXPECT_EQ(test().cols, 15);
+    EXPECT_THROW(std::cout << test(-1, 0), std::runtime_error);
+    EXPECT_THROW(std::cout << test(0, -1), std::runtime_error);
+    EXPECT_THROW(std::cout << test(10, 0), std::runtime_error);
+    EXPECT_THROW(std::cout << test(0, 15), std::runtime_error);
 }
+
+
+TEST(gtest, matrix_constructor)
+{
+    cv::Mat_<float> mat(10, 15, 1);
+    Image test(mat);
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 15; j++)
+            EXPECT_EQ(test(i, j), mat(i, j));
+    }
+}
+
+
+TEST(gtest, matrix_getter)
+{
+    cv::Mat_<float> mat(10, 15, 2);
+    Image test(mat);
+    cv::Mat_<float> pixels = test();
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 15; j++)
+            EXPECT_EQ(pixels(i, j), mat(i, j));
+    }
+
+    // Ne marche pas car problème de précision en mémoire pour les float
+    /* cv::Mat mat2 = cv::imread("../img/clean_finger.png", 0); */
+    /* cv::Mat_<float> tmp = mat2.clone(); */
+    /* Image test2("../img/clean_finger.png"); */
+    /* cv::Mat_<float> pixels2 = test2(); */
+
+    /* for (int i = 0; i < mat2.rows; i++) { */
+    /*     for (int j = 0; j < mat2.cols; j++) */
+    /*         EXPECT_EQ(pixels2(i, j), tmp(i, j) / 255.); */
+    /* } */
+}
+
 
 int main(int argc,char *argv[]) {
  ::testing::InitGoogleTest(&argc,argv);
