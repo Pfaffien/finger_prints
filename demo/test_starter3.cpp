c04e37ba0d86fd125b387816487d6e38220c9135
Clara <clara.bourgoin@grenoble-inp.org>
2020-01-13 11:13:22 +0100

    test

--- demo/test_starter3.cpp
+++ demo/test_starter3.cpp
@@ -21,16 +21,24 @@ using namespace std;
 int main(){
     Image finger("../img/clean_finger.png");
     finger.display("initial", "initial");
-    int size = 10;
-    cv::Mat_<float> dec = convolution_complex(finger(), size, finger().cols/2, finger().rows/2);
-    Image dec_img(dec);
-    dec_img.display("energy decreasing", "energy decreasing");
-    dec_img.save("Energy decreasing");
-    // finger.display("initial2", "initial2");
-    cv::Mat_<float> blur = convolution_complex(finger(), size, finger().cols/2, finger().rows/2, false);
-    Image blur_img(blur);
-    blur_img.display("blurring", "blurring");
-    blur_img.save("Blurring");
+    // int size = 10;
+    // cv::Mat_<float> dec = convolution_complex(finger(), size, finger().cols/2, finger().rows/2);
+    // Image dec_img(dec);
+    // dec_img.display("energy decreasing", "energy decreasing");
+    // dec_img.save("Energy decreasing");
+    // // finger.display("initial2", "initial2");
+    // cv::Mat_<float> blur = convolution_complex(finger(), size, finger().cols/2, finger().rows/2, false);
+    // Image blur_img(blur);
+    // blur_img.display("blurring", "blurring");
+    // blur_img.save("Blurring");
+    cv::Mat_<float> id(3,3, int(0));
+    id(1,1) = 1;
+    cv::Mat_<float> convDFT = convolutionDFT(finger(), id);
+    Image dft(convDFT);
+    dft.display("test", "test");
+
+    Image diff = finger - dft;
+    diff.display("diff", "diff");
 
     return 0;
 }
