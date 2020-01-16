dd996acc16aab109340e91ae7c1f4eb2c54e88ac
Thomas Bauer <thomas.bauer1@grenoble-inp.org>
2020-01-16 14:07:23 +0100

    Graphs of some functions

--- graphs.py
+++ graphs.py
@@ -9,20 +9,23 @@ C++ project
 """
 
 
-def main(x, f):
+def main(x, f, name):
     """
     Plot of some useful functions
     """
     plt.figure()
     plt.plot(x, f(x), color = "black")
-    plt.title("Selected function")
-    plt.savefig("img/saved/graph_selected_function.png")
+    plt.title("alpha = 1")
+    plt.savefig("img/saved/graph_function_{}.png".format(name))
     plt.show()
 
 
 if __name__ == "__main__":
     N = 10000
     x = np.linspace(0, 5, N)
-    f = lambda x: 1 - np.exp(- 1 / x**16)
-    selected = f(x)
-    main(x, f)
+    f = lambda x: 1 - np.exp(- 1 / x**2)
+    g = lambda x: np.exp(-x)
+    h = lambda x: 1 / (1 + x)
+    main(x, f, "selected")
+    main(x, g, "exp")
+    main(x, h, "inv")
