402aad4c149215f73cc4ec74e4ece4908fd1a029
bunders <s-buender@web.de>
2020-01-17 13:52:18 +0100

    small changes in starter2 main

--- demo/test_starter2.cpp
+++ demo/test_starter2.cpp
@@ -42,9 +42,20 @@ int main()
     //Rotate finger using backward interpolation
     Image rotate3 = finger.InverseRotation(0.25*M_PI);
     rotate3.display("Rotated finger 3");
+    rotate3.save("second_rotate");
 
     //Take a look at difference
     Image diff = rotate3 - rotate2;
     diff.display("Difference");
+    diff.save("diff_rotation_methods");
+    
+    //Rotate back to original position
+    Image rotate_back = rotate3.InverseRotation(1.75*M_PI);
+    rotate_back.display("Back and forth rotation with 2nd method");
+    
+    //Compute difference to original image
+    Image diff2 = rotate_back - finger;
+    diff2.save("diff_original_2nd_rotation");
+    
     return 0;
 }
