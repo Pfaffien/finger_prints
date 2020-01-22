2cd03c227860bd87101769cb5bc8a398c6d92aa4
Clara Bourgoin <clara.bourgoin@grenoble-inp.org>
2020-01-22 16:12:11 +0100

    starter 5

--- include/optimization.h
+++ include/optimization.h
@@ -3,5 +3,11 @@
 
 #include "image.h"
 
+float l_tx(Image, Image, int);
+int argmin_tx(Image, Image);
+
+float l_txy(Image, Image, int, int);
+void argmin_txy(Image, Image, int &, int &);
+
 
 #endif
