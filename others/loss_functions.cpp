3ebaaf2850c7fe61f43fdf5a26958291f4be43fd
Thomas Bauer <thomas.bauer1@grenoble-inp.org>
2020-01-22 15:50:58 +0100

    Graph of loss function tx

--- others/loss_functions.cpp
+++ others/loss_functions.cpp
@@ -4,19 +4,21 @@
 #include "image.h"
 #include "optimization.h"
 
+using namespace std;
+
 
 int main()
 {
-    const string fileName("../misc/data_transx.txt");
+    const string fileName("../others/data_transx.txt");
     ofstream file(fileName.c_str());
 
     if (file) {
         Image finger("../img/clean_finger.png");
         Image finger_tx("../img/tx_finger.png");
         int size = finger().cols;
-        file << 2 * size + 1 << std::endl;
-        for (int p = -size; p < size; p++)
-            file << l_tx(finger, finger_tx, p) << std::endl;
+        file << 2 * size - 1 << std::endl;
+        for (int p = -size+1; p < size; p++)
+            file << l_tx(finger_tx, finger, p) << std::endl;
     } else {
         std::cerr << "Error: could not open file" << std::endl;
     }
--- others/loss_functions.py
+++ others/loss_functions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
-from math import *
 
 """
 C++ project: graphs of loss functions
@@ -9,7 +8,24 @@ C++ project: graphs of loss functions
 
 
 def main():
-    pass
+    data = open("data_transx.txt", "r")
+    lines = data.readlines()
+    values = []
+
+    for line in lines:
+        values.append(line)
+    size, values = values[0], values[1:]
+    new_values = []
+    for elt in values:
+        new_values.append(float(elt[:-1]))
+
+    p = [i for i in range(-int(size)//2+1, int(size)//2+1)]
+
+    data.close()
+
+    plt.figure()
+    plt.plot(p, new_values)
+    plt.show()
 
 
 if __name__ == "__main__":
